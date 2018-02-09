7cac68c84547df41edb7b68bc307a5305bee33fe
venerari <35533668+venerari@users.noreply.github.com>
2018-02-08 23:39:06 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  subscription_id = "${var.access_appid}"
-  client_id       = "${var.access_clientid}"
+  subscription_id = "${var.access_subscriptid}"
+  client_id       = "${var.access_appid}"
   client_secret   = "${var.access_clientsecret}"
   tenant_id       = "${var.access_tenantid}"
 }
