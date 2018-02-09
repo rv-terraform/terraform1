6fb5367af76210d5a3e044471339c472470cfe7b
venerari <35533668+venerari@users.noreply.github.com>
2018-02-08 23:09:11 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable access_appid {
+    type = "xxxxxx"
+}
+
+variable access_clientid {
+    type = "xxxxxxx"
+}
+
+variable access_clientsecret {
+    type = "xxxxxx"
+}
+
+variable access_tenantid {
+    type = "xxxxxxx"
+}
