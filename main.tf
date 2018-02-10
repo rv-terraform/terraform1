b75a3e5ce89e35c7066b8fbbe0ecbdf46193bf27
venerari <35533668+venerari@users.noreply.github.com>
2018-02-10 18:08:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,12 +45,8 @@ resource "azurerm_virtual_machine" "test" {
   resource_group_name   = "${azurerm_resource_group.test.name}"
   network_interface_ids = ["${azurerm_network_interface.test.id}"]
   vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  delete_os_disk_on_termination = "true"
+  delete_data_disks_on_termination = "true"
 
   storage_image_reference {
     publisher = "Canonical"
