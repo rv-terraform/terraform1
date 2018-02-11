8bb66825314c447ca50839326f10ef942b0516c7
venerari <35533668+venerari@users.noreply.github.com>
2018-02-11 17:59:56 -0500

    Delete providers.tf

--- providers.tf
+++
@@ -1,8 +0,0 @@
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  subscription_id = "${var.access_subscriptid}"
-  client_id       = "${var.access_appid}"
-  client_secret   = "${var.access_clientsecret}"
-  tenant_id       = "${var.access_tenantid}"
-}
-
